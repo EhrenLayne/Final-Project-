5da3dbd06bbc7d4c1ad12bfd2ff6d903bb88d58c
jordanbecker1 <jordanbecker111@gmail.com>
2023-01-08 15:25:58 -0500

    changed df name

--- FinalProjectCode.R
+++ FinalProjectCode.R
@@ -1,9 +1,19 @@
 # R file
 
 install.packages("openxlsx")
+install.packages("readxl")
+
+library("readxl")
 library(openxlsx)
+library(tidyverse)
+library(readxl)
+library(here)
+
+library(tidyverse)
+library(lubridate)
+
+df <- read.xlsx("Marijuana_arrests_data.xlsx")
 
 
-Data <- read.xlsx("Marijuana_arrests_data.xlsx")
 
 head(Data)
