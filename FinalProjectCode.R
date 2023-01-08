39a70e1fa10551f1155977477f1c5b2446e4e5df
Jordan Becker <54598512+jordanbecker1@users.noreply.github.com>
2023-01-08 15:55:40 -0500

    Update FinalProjectCode.R

--- FinalProjectCode.R
+++ FinalProjectCode.R
@@ -1,14 +1,13 @@
 # R file
 
-install.packages("openxlsx")
-install.packages("readxl")
-
 library(readxl)
 library(openxlsx)
 library(tidyverse)
-library(readxl)
 library(here)
+
 
 df <- read.xlsx("Marijuana_arrests_data.xlsx")
 
-head(Data)
+
+
+head(df)
