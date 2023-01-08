ed2d8c046627228eab152378ed7d0e132f38b201
jordanbecker1 <jordanbecker111@gmail.com>
2023-01-08 16:44:04 -0500

    update

--- FinalProjectCode.R
+++ FinalProjectCode.R
@@ -11,5 +11,8 @@ df <- read.xlsx("Marijuana_arrests_data.xlsx")
 ggplot(df, aes(x=inc_rate, y= per_black, fill=inc_rate)) +  
   geom_line()
 
+ggplot(df, aes(x=inc_rate, y= per_black, fill=inc_rate)) +  
+  geom_point()
 
 head(df)
+glimpse(df)
